c0f9a90daa9a197533522d439a4df8c9a479cb53
fjruiz <pajarrurro@gmail.com>
2016-10-05 12:58:29 +0200

    Falta acabar 2

--- app/src/main/java/es/iesnervion/fjruiz/pruebasmayores/Principal.java
+++ app/src/main/java/es/iesnervion/fjruiz/pruebasmayores/Principal.java
@@ -1,22 +1,24 @@
 package es.iesnervion.fjruiz.pruebasmayores;
 
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class Principal extends AppCompatActivity implements View.OnClickListener {
+public class Principal extends AppCompatActivity implements View.OnClickListener
+                                                            ,RadioGroup.OnCheckedChangeListener{
 
-    Button sum;
+    Button sum,aliz,alde,disminuir,aumentar;
     EditText et1,et2,et3;
     TextView tv;
     RadioGroup rg;
-    RadioButton rbVerde,rbAzul,rbRojo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +29,18 @@ public class Principal extends AppCompatActivity implements View.OnClickListener
         et1=(EditText) findViewById(R.id.editText);
         et2=(EditText) findViewById(R.id.editText2);
         tv=(TextView) findViewById(R.id.Resultado);
+
+
         rg=(RadioGroup) findViewById(R.id.rg);
-        //rbVerde=(RadioButton) findViewById(R.id.rbVerde);
-        rbRojo=(RadioButton) findViewById(R.id.rbRojo);
-        rbAzul=(RadioButton) findViewById(R.id.rbAzul);
+        rg.setOnCheckedChangeListener(this);
         et3=(EditText) findViewById(R.id.editText3);
+
+        aliz=(Button) findViewById(R.id.alineaiz);
+        aliz.setOnClickListener(this);
+        alde=(Button) findViewById(R.id.alineade);
+        alde.setOnClickListener(this);
+
+        //disminuir
     }
 
     public void onClick(View v){
@@ -42,15 +51,27 @@ public class Principal extends AppCompatActivity implements View.OnClickListener
                         Double.parseDouble(et2.getText().toString());
                 tv.setText(res.toString());
                 return;
-            case R.id.rg:
-                switch (rg.getCheckedRadioButtonId()){
-                    case R.id.rbVerde:
-                        et3.setTextColor(2552550);
-                        return;
-
-                }
+            case R.id.alineaiz:
+                et3.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
                 return;
 
+            case R.id.alineade:
+                et3.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+                return;
+        }
+    }
+
+    public void onCheckedChanged(RadioGroup rg,int i){
+        switch (i){
+            case R.id.rbVerde:
+                et3.setTextColor(Color.GREEN);
+                return;
+            case R.id.rbAzul:
+                et3.setTextColor(Color.BLUE);
+                return;
+            case R.id.rbRojo:
+                et3.setTextColor(Color.RED);
+                return;
         }
     }
 }
